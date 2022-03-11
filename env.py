f77bdf0d82280b4b0806a605feae396b8169c94d
totalnoMartina <totalnobzvz@yahoo.com>
2022-03-11 12:36:49 +0000

    Add models, view, urls

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["SECRET_KEY"] = 'mysecret16'
+os.environ["DATABASE_URL"] = 'postgres://psxhiuvebpdxnk:11eda3c04297be98065364e0a6d80052aed1a4d50d5415e24e18ee3c32232794@ec2-54-216-185-51.eu-west-1.compute.amazonaws.com:5432/d3bimd42a4t610'
+os.environ["CLOUDINARY_URL"] = 'cloudinary://775846313975511:PhLXwT9VD42bkOq6rQYTw2RMC3c@dbfjmee9p'
--- mysite/settings.py
+++ mysite/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+import dj_database_url
+
+if os.path.isfile("env.py"):
+   import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -36,7 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
+    'dj_database_url',
+    'polls'
 ]
 
 MIDDLEWARE = [
@@ -73,11 +81,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
@@ -118,6 +130,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
